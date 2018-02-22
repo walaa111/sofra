98a523e2916636d43d2987a37d0174646c2fe9dd
walaa111 <walaagamil18@gmail.com>
2018-02-22 18:14:05 +0200

    w

--- sofra_app/www/js/index.js
+++ sofra_app/www/js/index.js
@@ -82,7 +82,7 @@ function initAd(){
                 overlap: true, // banner will overlap webview 
                 offsetTopBar: false, // set to true to avoid ios7 status bar overlap 
                 isTesting: false, // receiving test ad 
-                autoShow: false // auto show interstitial ad when loaded 
+                autoShow: true // auto show interstitial ad when loaded 
             });
  
             registerAdEvents();
@@ -100,8 +100,7 @@ function registerAdEvents() {
         document.addEventListener('onReceiveInterstitialAd', function(){ });
         document.addEventListener('onPresentInterstitialAd', function(){ });
         document.addEventListener('onDismissInterstitialAd', function(){ });
-        window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW
-        window.plugins.AdMob.requestInterstitialAd();
+        
     }
  //display the banner 
 function showBannerFunc(){
