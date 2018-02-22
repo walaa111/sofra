86f28540daf762cc05098ba4d71cb02398ba2426
walaa111 <walaagamil18@gmail.com>
2018-02-22 16:02:58 +0200

    w

--- sofra_app/www/js/index.js
+++ sofra_app/www/js/index.js
@@ -68,8 +68,8 @@ function initAd(){
                     interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
                 },
                 android : {
-                    banner: 'ca-app-pub-7251676025279948/8229545365',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-7251676025279948/6236030747'	//PUT ADMOB ADCODE HERE 
+                    banner: 'ca-app-pub-7251676025279948/7676925027',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-7251676025279948/3016050319'	//PUT ADMOB ADCODE HERE 
                 }
             };
             var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
